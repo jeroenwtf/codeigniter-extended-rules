1a7fc766ab05bde1cb033944d89bdd1e05612289
Jeroen van Meerendonk <jeroen@cyneek.com>
2013-06-20 14:31:59 +0200

    Nobody error_unexpects the spanish inquisition.

--- application/language/spanish/form_validation_lang.php
+++ application/language/spanish/form_validation_lang.php
@@ -1,23 +1,23 @@
 <?php
 
-$lang['file_required']			= "El campo %s es obligatorio.";
-$lang['file_size_max']			= "El archivo del campo %s es demasiado grande (el máximo permitido es %s).";
-$lang['file_size_min']			= "El archivo del campo %s es demasiado pequeño (el mínimo permitido es %s).";
-$lang['file_allowed_type']		= "El archivo del campo %s debería ser del tipo %s.";
-$lang['file_disallowed_type']	= "El archivo del campo %s no puede ser un %s.";
-$lang['file_image_maxdim']		= "El archivo del campo %s es demasiada grande.";
-$lang['file_image_mindim']		= "El archivo del campo %s es demasiada pequeño.";
-$lang['file_image_exactdim']	= "El archivo del campo %s no tiene las dimensiones adecuadas.";
-$lang['is_exactly']				= "El campo %s contiene un valor no válido.";
-$lang['is_not']					= "El campo %s contiene un valor no válido.";
-$lang['valid_hour']				= "El campo %s ha de contener una hora válida.";
+$lang['file_required']					= "El campo %s es obligatorio.";
+$lang['file_size_max']					= "El archivo del campo %s es demasiado grande (el máximo permitido es %s).";
+$lang['file_size_min']					= "El archivo del campo %s es demasiado pequeño (el mínimo permitido es %s).";
+$lang['file_allowed_type']				= "El archivo del campo %s debería ser del tipo %s.";
+$lang['file_disallowed_type']			= "El archivo del campo %s no puede ser un %s.";
+$lang['file_image_maxdim']				= "El archivo del campo %s es demasiada grande.";
+$lang['file_image_mindim']				= "El archivo del campo %s es demasiada pequeño.";
+$lang['file_image_exactdim']			= "El archivo del campo %s no tiene las dimensiones adecuadas.";
+$lang['is_exactly']						= "El campo %s contiene un valor no válido.";
+$lang['is_not']							= "El campo %s contiene un valor no válido.";
+$lang['valid_hour']						= "El campo %s ha de contener una hora válida.";
 $lang['error_max_filesize_phpini']		= "El archivo supera la directiva upload_max_filesize de php.ini.";
 $lang['error_max_filesize_form']		= "El archivo supera la directiva MAX_FILE_SIZE especificada en el formulario HTML.";
 $lang['error_partial_upload']			= "El archivo sólo se ha subido parcialmente.";
 $lang['error_temp_dir']					= "Error del directorio temporal.";
 $lang['error_disk_write']				= "Error de escritura en disco.";
 $lang['error_stopped']					= "Subida del archivo detenida por extensión.";
-$lang['error']							= "Error inesperado de subida de archivo. Error: %s";
+$lang['error_unexpected']				= "Error inesperado de subida de archivo. Error: %s";
 
 /* End of file form_validation_lang.php */
 /* Location: ./system/language/spanish/form_validation_lang.php */
